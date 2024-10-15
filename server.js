f0cb79f5a4ead63a2c591727f07699a5e6fafcf9
eshwar <eshwarrachakonda02@gmail.com>
2024-10-16 02:59:28 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const admin = require('firebase-admin');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URI)
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.log(err));
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
